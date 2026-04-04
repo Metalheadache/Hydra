e43b88987668bd923390271ea26c30bb17de18e1
Metalheadache <wyztonywyz@hotmail.com>
2026-04-04 23:37:01 +0800

    v1.0.1: README updates for PyPI

--- hydra_agents/__init__.py
+++ hydra_agents/__init__.py
@@ -38,7 +38,7 @@ from hydra_agents.tool_registry import ToolRegistry
 
 logger = structlog.get_logger(__name__)
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __all__ = ["Hydra", "HydraConfig", "EventBus", "EventType", "HydraEvent"]
 
 
